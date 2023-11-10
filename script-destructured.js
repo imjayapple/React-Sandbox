8628cdef43958c601c4bd83337a28302789d624d
imjayapple <imjayapple@gmail.com>
2023-11-10 11:46:49 -0800

    Destructring .js refresher

---
+++ script-destructured.js
@@ -0,0 +1,5 @@
+//Shorten the code to make it more concise
+let cars = ['ferrari', 'tesla', 'honda'];
+let [car1, car2, car3] = cars;
+
+console.log(car1, car2, car3);
---
+++ script.js
@@ -0,0 +1,10 @@
+//Build a simple array
+let cars = ['ferrari', 'tesla', 'honda'];
+
+let car1 = cars[0];
+let car2 = cars[1];
+let car3 = cars[2];
+
+console.log(car1, car2, car3); // Prints: ferrari tesla honda
+
+//Simplify the arrary → 'script-destructured.js'
