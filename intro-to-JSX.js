598955e87f09f1cd3879c687266e930804eb976c
imjayapple <imjayapple@gmail.com>
2023-11-11 22:54:58 -0800

    Intro to JSX file added, breaking down syntax expressions

---
+++ intro-to-JSX.js
@@ -0,0 +1,13 @@
+// JSX is a syntax extension for JavaScript.
+const h1 = <h1>Hello world</h1>;
+
+// What is a "syntax extension"?
+<p>Hola world</p>
+
+const mySquad = {
+    center: <li>Timbo</li>,
+    powerForward: <li>Ramone</li>,
+    smallForward: <li>Benito</li>,
+    shootingGuard: <li>Lukas</li>,
+    pointGuard: <li>Saunder</li>
+  };
